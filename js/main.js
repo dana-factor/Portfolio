3474dbdbffcfeda27b1d826daed8ba562cead27d
dana-factor <factor.dana@gmail.com>
2020-06-01 23:04:00 +0300

    updating links to projects

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@ var gProjs = [{
     name: "Mine-Sweeper",
     title: "Try not to blow up",
     desc: "The objective of the game is to clear a board containing hidden mines without detonating any of them, with help from clues about the number of neighboring mines in each field.",
-    url: "projs/mine-sweeper",
-    publishedAt: 1448693940000,
-    labels: ["Matrixes", "keyboard events"]
+    url: "https://dana-factor.github.io/Mine-Sweeper-Project-/",
+    publishedAt: 'May 2020',
+    labels: ["Matrixes", "Mouse events"]
 }, {
     id: "in-picture",
     name: "Guess The Flag",
     title: "Are u the king of the world?",
     desc: "The objective of the game is to recognize the countries only by their flags",
-    url: "projs/in-picture",
-    publishedAt: 1448693940000,
-    labels: ["Matrixes", "keyboard events"]
+    url: "https://dana-factor.github.io/in-picture/",
+    publishedAt: 'May 2020',
+    labels: ["game", "CSS"]
 
 }, {
     id: "books-shop",
@@ -22,8 +22,8 @@ var gProjs = [{
     title: "Manage your stock",
     desc: "An app to help you manage your book-shoop, add/ remove books, update prices, review ratings and get all the info you need",
     url: "projs/books-shop",
-    publishedAt: 1448693940000,
-    labels: ["Matrixes", "keyboard events"]
+    publishedAt: 'May 2020',
+    labels: ["MVC", "CRUDL"]
 }]
 
 $(document).ready(init);
@@ -36,7 +36,7 @@ function init() {
 function renderProjs() {
     var strHTMLs = []
     gProjs.forEach(function (proj) {
-        var strHTML = `<div class="col-md-4 col-sm-6 portfolio-item" onclick="renderModal('${proj.name}', '${proj.desc}', '${proj.id}', '${proj.title}')">
+        var strHTML = `<div class="col-md-4 col-sm-6 portfolio-item" onclick="renderModal('${proj.name}', '${proj.desc}', '${proj.id}', '${proj.title}', '${proj.url}', '${proj.publishedAt}', '${proj.labels.join(' ')}')">
          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">  
         <div class="portfolio-hover">
               <div class="portfolio-hover-content">
@@ -56,7 +56,7 @@ function renderProjs() {
 }
 
 
-function renderModal(projName, projDesc, projId, projTitle) {
+function renderModal(projName, projDesc, projId, projTitle, projURL, projPublished, projLables) {
     var strHTML = `<div class="portfolio-modal modal fade show" id="portfolioModal1" style="display: block" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -69,14 +69,14 @@ function renderModal(projName, projDesc, projId, projTitle) {
           <div class="row">
             <div class="col-lg-8 mx-auto">
               <div class="modal-body">
-                <h2>${projName}</h2>
+              <a href=${projURL} target="_blank"><h2>${projName}</h2></a>
                 <p class="item-intro text-muted">${projDesc}</p>
                 <img class="img-fluid d-block mx-auto" src="img/portfolio/${projId}-full.png" alt="">
                 <p>${projTitle}</p>
                 <ul class="list-inline">
-                  <li>Date: May 2020</li>
-                  <li>Client: Threads</li>
-                  <li>Category: Games</li>
+                  <li>Date: ${projPublished}</li>
+                  <li><a href=${projURL} target="_blank">explore</a></li>
+                  <li>Category: ${projLables}</li>
                 </ul>
                 <button class="btn btn-primary" data-dismiss="modal" type="button">
                     <i class="fa fa-times"></i> Close Project</button>
